a0955e0406f2581d95eb2a2302c25b0e48390f37
yourfather <okuhlexesibe0@gmail.com>
2025-07-03 23:35:45 +0200

    I did slices today

--- hello-world.go
+++ hello-world.go
@@ -2,46 +2,84 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 func main() {
 
-	var a  [5]int
 
-	fmt.Println("emp: ", a)
+	var s []string
 
-	a[4] = 100
+	fmt.Println("uninit: ", s, s ==nil , len(s)==0)
 
-	fmt.Println("set: ", a)
-	fmt.Println("ge: ",a[4])
+	s = make([]string, 3)
 
-	fmt.Println("len: ", len(a))
+	fmt.Println("empl:", s , " len:", len(s), " cap:", cap(s))
 
-	b := [5]int{1 , 2, 3, 4, 5}
-	fmt.Println("dcl: ", b)
+	s[0] = "a"
+    s[1] = "b"
+    s[2] = "c"
 
-	b = [...]int{1 , 2, 3, 4, 5}
+	fmt.Println("set:", s)
+    fmt.Println("get:", s[2])
 
-	fmt.Println("dcl: ", b)
+	fmt.Println("len: ", len(s))
+	
+	s = append(s, "d")
+	s = append(s, "e", "f", "g")
 
-	b = [...]int{100, 3 : 400 ,500}
-	fmt.Println("idx: ", b)
+	fmt.Println("new len: ", len(s))
+	fmt.Println("new cap: ", cap(s))
 
-	var twoD [2][3]int
+	fmt.Println(cap(s)-len(s))
 
-	for i := range 2{ 
-		for j := range 3{
-			twoD[i][j] = i + j 
+	c := make([]string , len(s))
+	copy(c,s)
+	fmt.Println("This is how c looks like now: ", c)
+
+	l := s[2:5] //This one takes from 2 to before 5 ... yes including 2 but excluding 5.
+
+	fmt.Println("This is the 1st 'slice': ", l )
+
+	l = s[:5] //takes all to before 5 ... yes excluding 5
+
+	fmt.Println("This is the teh 2nd slice: ", l)
+
+	l = s[2:]
+
+	fmt.Println("This is teh last slice: ", l) // thisfrom takes all after and including 2
+
+	t :=[]string{"1 ", "2 ", "3 "}
+
+	fmt.Println("Look i initiated and declared  a slice and called it =>", t)
+
+	t2 := make([]string , len(t))
+
+	copy(t2, t)
+
+	if slices.Equal(t, t2) { 
+
+		fmt.Println("Hey look these guys are equal: ")
+	}
+
+
+	//We now look at the almighty 2D slices
+
+	twoD := make([][] int , 3)
+	
+	for i := range 3 { 
+		innerlen := i+1
+		twoD[i] = make([]int,innerlen )
+		
+		for j := range innerlen{
+
+			twoD[i][j] = i + j
 		}
 	}
-	fmt.Println("2d: ", twoD)
-	
-	twoD =[2][3]int { 
 
-		{1,2,3},
-		{4,5,6}, 
-	}
+	fmt.Println("Hey look i made a 2D , how cool is that => " , twoD)
 
-	fmt.Println("dcl2D: ", twoD)
+
+
 }
 
